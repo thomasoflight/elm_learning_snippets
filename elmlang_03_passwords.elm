228f22625702c4152db44c81c34082596aaf8977
A.M. Thomas <thomasoflight@lightfaced.org>
2017-12-30 15:31:51 -0500

    completed additional exercises

--- elmlang_03_passwords.elm
+++ elmlang_03_passwords.elm
@@ -2,17 +2,19 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-import String exposing (any, contains, join, isEmpty)
+import Html.Events exposing (..)
+import String exposing (..)
 import Char exposing (isUpper, isLower, isDigit)
 import List
+import Result
 
 
 main =
-    Html.beginnerProgram
-        { model = model
+    Html.program
+        { init = ( model, Cmd.none )
         , view = view
         , update = update
+        , subscriptions = (\model -> Sub.none)
         }
 
 
@@ -20,12 +22,14 @@ type alias Model =
     { name : String
     , password : String
     , passwordRepeat : String
+    , age : String
+    , submitted : Bool
     }
 
 
 model : Model
 model =
-    Model "" "" ""
+    Model "" "" "" "" False
 
 
 
@@ -37,19 +41,27 @@ type Msg
     = Name String
     | Password String
     | PasswordRepeat String
+    | Age String
+    | Submit Bool
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Name name ->
-            { model | name = name }
+            ( { model | name = name }, Cmd.none )
 
         Password password ->
-            { model | password = password }
+            ( { model | password = password }, Cmd.none )
 
         PasswordRepeat password ->
-            { model | passwordRepeat = password }
+            ( { model | passwordRepeat = password }, Cmd.none )
+
+        Age age ->
+            ( { model | age = age }, Cmd.none )
+
+        Submit bool ->
+            ( { model | submitted = bool }, Cmd.none )
 
 
 view : Model -> Html Msg
@@ -58,7 +70,13 @@ view model =
         [ input [ type_ "text", placeholder "Name", onInput Name ] []
         , input [ type_ "text", placeholder "Password", onInput Password ] []
         , input [ type_ "text", placeholder "Retype Password", onInput PasswordRepeat ] []
-        , viewValidation model
+        , input [ type_ "text", placeholder "Age", onInput Age ] []
+        , button [ type_ "submit", onClick (Submit True) ] [ text "submit" ]
+        , (if model.submitted == True then
+            viewValidation model
+           else
+            div [] []
+          )
         ]
 
 
@@ -66,7 +84,13 @@ viewValidation : Model -> Html Msg
 viewValidation model =
     let
         ( color, isMatch ) =
-            if List.any isEmpty [ model.password, model.name ] then
+            if
+                List.any isEmpty
+                    [ model.password
+                    , model.name
+                    , model.age
+                    ]
+            then
                 ( "pink", "one or more fields missing my friend" )
             else if model.password == model.passwordRepeat && not allConditionsMet then
                 ( "blue", "passwords match but... other stuff is off" )
@@ -95,8 +119,19 @@ viewValidation model =
             else
                 ":)"
 
+        requiredAgeFormat =
+            if isEmpty model.age || not (all isDigit model.age) then
+                "Needs numbers only, please"
+            else
+                ":)"
+
         allConditionsMet =
-            List.all (contains ":)") [ requiredLength, requiredChars, requiredDigits ]
+            List.all (contains ":)")
+                [ requiredLength
+                , requiredChars
+                , requiredDigits
+                , requiredAgeFormat
+                ]
     in
         div []
             [ h3 [ style [ ( "color", color ) ] ] [ text isMatch ]
@@ -104,5 +139,6 @@ viewValidation model =
                 [ li [] [ text requiredLength ]
                 , li [] [ text requiredChars ]
                 , li [] [ text requiredDigits ]
+                , li [] [ text requiredAgeFormat ]
                 ]
             ]
